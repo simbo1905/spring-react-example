99d3c1ad433020aca3b69abffc76bbfe8cc35ec1
simbo1905 <simbo1905@60hertz.com>
2018-02-14 11:17:18 +0000

    CompiledScript alternative approach and some timings over 100 runs of each.

--- src/main/java/com/winterbe/react/React.java
+++ src/main/java/com/winterbe/react/React.java
@@ -2,14 +2,42 @@ package com.winterbe.react;
 
 import jdk.nashorn.api.scripting.NashornScriptEngine;
 
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
+import javax.script.*;
+import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class React {
+
+    private ThreadLocal<CompiledScript> scriptHolder = new ThreadLocal<CompiledScript>(){
+        @Override
+        protected CompiledScript initialValue() {
+
+            String script = Stream.of("static/nashorn-polyfill.js",
+                    "static/vendor/react.js",
+                    "static/vendor/showdown.min.js",
+                    "static/commentBox.js")
+                    .map(ThrowingFunction.wrap(React::slurp)).collect(Collectors.joining());
+
+            try {
+                ScriptEngine engine =  new ScriptEngineManager().getEngineByName("JavaScript");
+                Compilable compilingEngine = (Compilable) engine;
+                CompiledScript cs = compilingEngine.compile(script);
+
+                Bindings bindings = engine.getBindings(ScriptContext.ENGINE_SCOPE);
+                cs.eval(bindings);
+                return cs;
+            } catch (ScriptException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    };
 
     private ThreadLocal<NashornScriptEngine> engineHolder = new ThreadLocal<NashornScriptEngine>() {
         @Override
@@ -37,8 +65,28 @@ public class React {
         }
     }
 
+    public  String compiledRenderCommentBox(List<Comment> comments) {
+        try {
+            CompiledScript cscript = scriptHolder.get();
+
+            Invocable invocable = (Invocable) cscript.getEngine();
+
+            return String.valueOf(invocable.invokeFunction("renderServer", comments));
+        }
+        catch (Exception e) {
+            throw new IllegalStateException("failed to render react component", e);
+        }
+    }
+
     private Reader read(String path) {
         InputStream in = getClass().getClassLoader().getResourceAsStream(path);
         return new InputStreamReader(in);
+    }
+
+    public static String slurp(String path) throws java.io.IOException {
+        try (InputStream resource = React.class.getClassLoader().getResourceAsStream(path)) {
+            return String.join("\n", new BufferedReader(new InputStreamReader(resource,
+                    StandardCharsets.UTF_8)).lines().collect(Collectors.toList())) + "\n";
+        }
     }
 }
--- src/test/java/com/winterbe/react/ReactTest.java
+++ src/test/java/com/winterbe/react/ReactTest.java
@@ -5,16 +5,24 @@ import static org.hamcrest.Matchers.startsWith;
 import static org.junit.Assert.assertThat;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.junit.Test;
 
+import javax.script.*;
+
 public class ReactTest {
 
     @Test
     public void testRenderCommentBox() throws Exception {
+
+        System.out.println("-------------");
         List<Comment> comments = new ArrayList<>();
         comments.add(new Comment("Peter Parker", "This is a comment."));
         comments.add(new Comment("John Doe", "This is *another* comment."));
@@ -26,6 +34,46 @@ public class ReactTest {
 
         Document doc = Jsoup.parse(html);
         assertThat(doc.select("div.comment").size(), is(2));
+
+        for( int i = 0; i < 100;i++ ) {
+            long startTime = System.nanoTime();
+            react.renderCommentBox(comments);
+            long endTime = System.nanoTime();
+            long duration = (endTime - startTime);
+            double milli = duration / 1e6;
+            System.out.println(String.format("%.2f", milli));
+        }
     }
 
+    @Test
+    public void testCompiledRenderCommentBox() throws Exception {
+
+        System.out.println("-------------");
+        String script = Stream.of("static/nashorn-polyfill.js",
+                "static/vendor/react.js",
+                "static/vendor/showdown.min.js",
+                "static/commentBox.js")
+                .map(ThrowingFunction.wrap(React::slurp)).collect(Collectors.joining());
+
+        List<Comment> comments = new ArrayList<>();
+        comments.add(new Comment("Peter Parker", "This is a comment."));
+        comments.add(new Comment("John Doe", "This is *another* comment."));
+
+        React react = new React();
+        String html = react.compiledRenderCommentBox(comments);
+
+        assertThat(html, startsWith("<div"));
+
+        Document doc = Jsoup.parse(html);
+        assertThat(doc.select("div.comment").size(), is(2));
+
+        for( int i = 0; i < 100;i++ ) {
+            long startTime = System.nanoTime();
+            react.compiledRenderCommentBox(comments);
+            long endTime = System.nanoTime();
+            long duration = (endTime - startTime);
+            double milli = duration / 1e6;
+            System.out.println(String.format("%.2f", milli));
+        }
+    }
 }
